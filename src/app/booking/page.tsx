ea80ea7783994b70a5d847994c0597ff70c6618a
daniel <danieldanyg@gmail.com>
2023-11-21 02:27:04 -0500

    Added tables for meetings and subscription pages

--- src/app/booking/page.tsx
+++ src/app/booking/page.tsx
@@ -1,6 +1,8 @@
 'use client'
 import React from 'react'
 import NavBar from '../components/NavBar';
+import BookingsTable from '../components/BookingsTable';
+import { columns, meetings } from './bookingData';
 
 type Props = {}
 
@@ -8,6 +10,11 @@ export default function Booking({}: Props) {
   return (
     <>
       <NavBar pageName='Booking'/>
+      <div className='py-24'>
+        <div className='container m-auto'>
+          <BookingsTable columns={columns} meetings={meetings}/>  
+        </div>
+      </div>
     </>
   )
 }
